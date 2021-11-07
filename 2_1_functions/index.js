fc103790df758069cc886899410cbc0861beccfb
revital-m <revital13m@gmail.com>
2021-11-07 12:35:05 +0200

    exercise 2.1 - done

---
+++ 2_1_functions/index.js
@@ -0,0 +1,11 @@
+function describeCountry(country, population, capitalCity){
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+};
+
+let var1 = describeCountry('Israel', 9, 'Jerusalem');
+let var2 = describeCountry('Spain', 46, 'Madrid');
+let var3 = describeCountry('France', 65, 'Paris');
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
