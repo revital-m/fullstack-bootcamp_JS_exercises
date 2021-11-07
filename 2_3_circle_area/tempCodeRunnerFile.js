fe0573be42d830fe6712d0529f1ca057bb22b1ad
revital-m <revital13m@gmail.com>
2021-11-07 21:02:25 +0200

    exercise 2.4 - done

---
+++ 2_3_circle_area/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(myArea);
+// console.log(myAreaRounded);
--- 2_4_date/index.js
+++ 2_4_date/index.js
@@ -1,7 +1,10 @@
 function currentDate() {
-    let today = new Date();
     
-    return `Today is ${today.toDateString()}`
+    let day = ['Sunday', 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday'];
+    let month = ['January' , 'February' , 'March' , 'April' , 'May' , 'June' , 'July' , 'August' , 'September' , 'October' , 'November' , 'December'];
+    let date = new Date();
+
+    return  `Today is ${day[date.getDay()]} the ${date.getDate()} of ${month[date.getMonth()]}, ${date.getFullYear()}`;
 };
 
 let today = currentDate();
