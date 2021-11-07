3f153c789d717801ebd47991c4afb00168a32c09
revital-m <revital13m@gmail.com>
2021-11-07 11:30:01 +0200

    exercise 1.1 - done

---
+++ 1_1_primitive_data_types/index.js
@@ -0,0 +1,18 @@
+let str = "this is a string";
+let num = 13;
+let obj = {
+    var1: 35,
+    var2: "string"
+};
+let bool = true;
+let empty = null;
+let notDefined;
+let sym = Symbol('try');
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof obj);
+console.log(typeof bool);
+console.log(typeof empty);
+console.log(typeof notDefined);
+console.log(typeof sym);
