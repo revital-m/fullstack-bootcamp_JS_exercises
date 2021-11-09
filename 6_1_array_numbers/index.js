e4a8ce27ca5f65b0d18899c00dd15b38b298e357
revital-m <revital13m@gmail.com>
2021-11-09 12:31:32 +0200

    exercise 6.1 - done

---
+++ 6_1_array_numbers/index.js
@@ -0,0 +1,37 @@
+const arr = [1,7,3,0,-5,7,3,9];
+
+(function arrPrint(arr){
+    console.log('arr ='); 
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]); 
+    }
+    return 0;
+})(arr);
+
+(function arrLength(arr){
+    let myArrLength = 0;
+    for (let i = 0; i < arr.length; i++) {
+        myArrLength ++;
+    }
+    console.log(`myArrLength = ${myArrLength}`); 
+    return myArrLength;
+})(arr);
+
+(function arrSum(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(`sum = ${sum}`); 
+    return sum;
+})(arr);
+
+(function arrMulti(arr){
+    let mult = 0;
+    for (let i = 0; i < arr.length; i++) {
+        mult *= arr[i];
+    }
+    console.log(`mult = ${mult}`); 
+    return mult;
+})(arr);
+
