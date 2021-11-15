6ee09edee381446332472d9587743da376c698b8
revital-m <revital13m@gmail.com>
2021-11-15 10:41:04 +0200

    extra - 8.1

---
+++ extra_8_1_maps_with_objects/index.js
@@ -0,0 +1,10 @@
+let myObj1 = {name: 'This is obj 1.'};
+let myObj2 = {name: 'This is obj 2.'};
+let myObj3 = {name: 'This is obj 3.'};
+
+let myMap = new Map([[myObj1,1], [myObj2,2], [myObj3,3]]);
+console.log(myMap);
+
+for (const el of myMap) {
+    console.log(el);
+}
