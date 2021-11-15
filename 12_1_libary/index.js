66d4ebd578f3d5777d95821707ea4b55a3435543
revital-m <revital13m@gmail.com>
2021-11-15 15:14:51 +0200

    exercise 12.1 - done

---
+++ 12_1_libary/index.js
@@ -0,0 +1,24 @@
+var library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+function canBeRead(arr){
+    return arr.filter(element => (element.readingStatus))
+}
+
+let bookArr = canBeRead(library);
+console.log(`bookArr = `,bookArr);
