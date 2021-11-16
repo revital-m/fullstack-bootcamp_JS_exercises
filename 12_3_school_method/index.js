f71fab0bb91e74b0cf673380485d29e0c2e6ec06
revital-m <revital13m@gmail.com>
2021-11-16 12:10:45 +0200

    exercise 12.3 - done

--- 12_3_school_method/index.js
+++ 12_3_school_method/index.js
@@ -41,9 +41,57 @@ const school = {
 
 
 function findPerson(typeOfPerson, id){
-    return school[typeOfPerson].find(person => (person.id === id));
+  return typeOfPerson.find(person => (person.id === id));
 }
 
-
-let findPersonInObj = findPerson(students, 10);
+let findPersonInObj = findPerson(school.teachers, 1);
 console.log(`findPersonInObj = `,findPersonInObj);
+
+
+function assignStudent(id, subject){
+  let optionalTeacher = [];
+  school.teachers.forEach(teacher => {
+    teacher.subjects.forEach(mySubject => {
+      if (mySubject === subject) {
+        optionalTeacher.push(teacher);
+      }
+    });
+  });
+  
+  let isFull = optionalTeacher.find(teacher => (teacher.capacityLeft > 0));
+  if (!isFull) {return 'Sorry, no available teachers left.';}
+  let student = findPerson(school.students, id);
+  if (!student) {return 'No such student exist.';}
+  isFull.students.push(student);
+  isFull.capacityLeft--;
+}
+
+let assignStudentInObj = assignStudent(10, 'chemistry');
+console.log(`assignStudentInObj = `,school);
+
+function assignTeachersSubject(id, newSubject){
+  let myTeacher = findPerson(school.teachers, id);
+  if (!myTeacher) {return 'No such teacher exist.';}
+  isSubject = myTeacher.subjects.find(subject => (subject === newSubject));
+  if (isSubject) {return `The subject- ${newSubject} already exist.`;}
+  myTeacher.subjects.push(newSubject);
+  return myTeacher;
+}
+
+let newSubjectInObj = assignTeachersSubject(2, 'chemistry');
+console.log(`newSubjectInObj = `,newSubjectInObj);
+
+function addNewStudent(studentsArr, name, id, age){
+  let isExist = findPerson(studentsArr, id);
+  if (isExist) {return `The student- ${name} already exist.`;}
+  studentsArr.push({
+    id,
+    name,
+    age,
+  });
+  return studentsArr;
+}
+
+let newStudentInObj = addNewStudent(school.students, 'Lily',14, 16);
+console.log(`newStudentInObj = `,newStudentInObj);
+console.log(school);
