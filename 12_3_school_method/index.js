3035ddb547a9711f494aeae2e389c187d32cb557
revital-m <revital13m@gmail.com>
2021-11-16 10:05:12 +0200

    exercise 12.3

---
+++ 12_3_school_method/index.js
@@ -0,0 +1,49 @@
+const school = {
+  teachers: [
+    {
+      id: 1,
+      name: "Pinchas",
+      subjects: ["chemistry", "biology", "physics"],
+      students: [],
+      capacityLeft: 3,
+    },
+    {
+      id: 2,
+      name: "Williams",
+      subjects: ["history", "ethics"],
+      students: [],
+      capacityLeft: 2,
+    },
+  ],
+  students: [
+    {
+      id: 10,
+      name: "Jennifer",
+      age: 20,
+    },
+    {
+      id: 11,
+      name: "Howard",
+      age: 23,
+    },
+    {
+      id: 12,
+      name: "Old-Timmy",
+      age: 86,
+    },
+    {
+      id: 13,
+      name: "Houston",
+      age: 21,
+    },
+  ],
+};
+
+
+function findPerson(typeOfPerson, id){
+    return school[typeOfPerson].find(person => (person.id === id));
+}
+
+
+let findPersonInObj = findPerson(students, 10);
+console.log(`findPersonInObj = `,findPersonInObj);
