0e69c35a4b8f17e67a7c5954a29d023f41f3b135
revital-m <revital13m@gmail.com>
2021-11-20 22:43:34 +0200

    ex 15.1 - done

--- 15_1_get_sum/index.js
+++ 15_1_get_sum/index.js
@@ -1,3 +1,4 @@
+/*
 function getSum(arr1, arr2) {
   const sum = 0;
   for (let i = 0; i < arr1.length; i++) {
@@ -8,9 +9,29 @@ function getSum(arr1, arr2) {
   }
 }
 getSum([1, 2, 3][(5, 66, 23)]);
-
+*/
 
 //? Q-1
+//* the problem was at lines 2 & 11.
+//* const sum = 0;
+//* getSum([1,2,3][5,66,23]);
+
 //? Q-2
+//* the debuger in chrome.
+
 //? Q-3
+//* in line 2: you can not change a const variable.
+//* in line 11: forgot a comma between the two arrays.
+
 //? Q-4
+function getSum(arr1, arr2) {
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  }
+  return sum;
+}
+console.log(getSum([1, 2, 3], [(5, 66, 23)]));
