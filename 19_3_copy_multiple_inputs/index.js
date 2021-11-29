9f05e56bc19a7cb390551cc73f9c2d0b43d69e45
revital-m <revital13m@gmail.com>
2021-11-29 16:09:21 +0200

    ex 19.3 - done

--- 19_3_copy_multiple_inputs/index.js
+++ 19_3_copy_multiple_inputs/index.js
@@ -1,5 +1,17 @@
-function clickEvent(first,last){
-    if (first.value.length) {
-        document.querySelector(`#${last}`).focus();
-    }
-}
+const userInputs = document.querySelectorAll('input');
+
+userInputs.forEach((input,idx) => {
+    input.addEventListener('paste', (event) => {
+        const txt = event.clipboardData.getData('text');
+        const txtArr = txt.split('');
+        for (let i = 0; i < txtArr.length; i++) {
+            userInputs[idx + i].value = txtArr[i];
+        }
+    });
+
+    input.addEventListener('keyup', () => {
+        if (input.nextElementSibling && input.value) {
+            input.nextElementSibling.focus();
+        }
+    });
+});
