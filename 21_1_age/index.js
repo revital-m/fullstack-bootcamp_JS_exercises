00560d886aff052183a1cd873e02e5411f325326
revital-m <revital13m@gmail.com>
2021-11-29 18:40:07 +0200

    ex 21.1 - done

--- 21_1_age/index.js
+++ 21_1_age/index.js
@@ -1,28 +1,14 @@
-let age = document.querySelector('input[type="text"]');
-let btn = document.querySelector('button');
-let res = document.querySelector('.text');
+const userAge = document.querySelector("input");
+const txt = document.querySelector(".text");
+const btn = document.querySelector("button");
 
-
-
-age.addEventListener('keyup',(el) => {
-    btn.addEventListener('click', (userAge) => {
-        if (age.value >= '18') {
-            res.innerText = 'you can drink';
-            // age.value= 'you can drink';
-            // console.log("you can drink");
-        }
-        else {
-            // age.value= "you’re too young";
-            res.innerText = 'you’re too young';
-        }
-    });
-    btn.addEventListener('click', (userAge) => {
-        if (age.value < '18') {
-            res.innerText = 'you’re too young';
-            // age.value= "you’re too young";
-            // console.log("you’re too young");
-        }
-    });
+btn.addEventListener("click", () => {
+  if (userAge.value < 18) {
+    txt.innerText = "you’re too young";
+  } else {
+    txt.innerText = "you can drink";
+    const cheers = document.createElement("i");
+    cheers.classList.add("fas", "fa-glass-cheers");
+    txt.appendChild(cheers);
+  }
 });
-
-// console.dir(age);
