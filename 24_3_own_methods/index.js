b88250e14ac5a7ce113adb9c4a5e6ced698446bb
revital-m <revital13m@gmail.com>
2021-12-05 15:45:37 +0200

    ex 24.3 - done

--- 24_3_own_methods/index.js
+++ 24_3_own_methods/index.js
@@ -1,35 +1,35 @@
-let array = [3,61,5,43,7,8,5,54,77];
+let array = [3, 61, 5, 43, 7, 8, 5, 54, 77];
 
 //filter method
-Array.prototype.myFilter = function(){
-    let arr=[];
-    for(let i = 0; i < this.length; i++){
-        if (this[i] % 2 === 0) {
-            arr.push(this[i]);
-        } 
+Array.prototype.myFilter = function (callBackFunction) {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callBackFunction(this[i])) {
+      arr.push(this[i]);
     }
-return arr;
-}
-let arr = array.myFilter();
+  }
+  return arr;
+};
+let arr = array.myFilter((element) => element % 2 === 0);
 console.log(arr);
 
 //find method
-Array.prototype.myFind = function(num){
-    for(let i = 0; i < this.length; i++){
-        if (this[i]=== num) {
-            return true;
-        } 
+Array.prototype.myFind = function (num) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === num) {
+      return true;
     }
-    return false;
-}
-console.log(array.myFind(54));
+  }
+  return false;
+};
+console.log(array.myFind(43));
 
 //reduce method
-Array.prototype.myReduce = function(){
-    let prev = this[0];
-    for(let i = 1; i < this.length; i++){
-        prev += this[i];
-    }
-    return prev;
-}
-console.log(array.myReduce());
+Array.prototype.myReduce = function (callBackFunction) {
+  prev = this[0];
+  for (let i = 1; i < this.length; i++) {
+    prev = callBackFunction(prev, this[i]);
+  }
+  return prev;
+};
+console.log(array.myReduce((prev, curr) => (prev += curr)));
