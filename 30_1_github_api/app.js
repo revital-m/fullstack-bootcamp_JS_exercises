661cba321a13d071b68c6ea58cb13552b42acb2d
revital-m <revital13m@gmail.com>
2021-12-08 19:55:20 +0200

    change error message

--- 30_1_github_api/app.js
+++ 30_1_github_api/app.js
@@ -6,74 +6,74 @@ const errText = document.querySelector("#errMsg");
 
 const usersArr = [];
 
-window.addEventListener('load', () => {
-    inputText.focus();
+window.addEventListener("load", () => {
+  inputText.focus();
 });
 
 searchBtn.addEventListener("click", (e) => {
-    userInput = inputText.value;
-    inputText.value = "";
-  if (userInput !== "") {
+  userInput = inputText.value;
+  inputText.value = "";
+  if (userInput !== "" && !usersArr.includes(`${userInput}`)) {
     newUser();
   }
 });
 inputText.addEventListener("keypress", (e) => {
-    if (!errText.classList.contains('hiddenText')) {
-        errText.classList.add('hiddenText');
-    }
-    userInput = inputText.value;
-  if (e.key === "Enter" && userInput !== "") {
+  if (!errText.classList.contains("hiddenText")) {
+    errText.classList.add("hiddenText");
+  }
+  userInput = inputText.value;
+  if (
+    e.key === "Enter" &&
+    userInput !== "" &&
+    !usersArr.includes(`${userInput}`)
+  ) {
     inputText.value = "";
     newUser();
   }
 });
 
-async function newUser() { 
-    try {
-        const res = await fetch(`https://api.github.com/users/${userInput}`);
-        const data = await res.json();
-        if (res.ok) {
-            newCard(data);
-        }
-        else {
-            errMsg(`'${userInput}' is an invalid username!`);
-        }
-    } 
-    catch(e) {
-        errMsg(err);
+async function newUser() {
+  try {
+    const res = await fetch(`https://api.github.com/users/${userInput}`);
+    const data = await res.json();
+    if (res.ok) {
+      newCard(data);
+    } else {
+      throw new Error(data.message);
     }
+  } catch (e) {
+    errMsg(e);
+  }
 }
 
-function newCard(data){
-    if (!usersArr.includes(`${userInput}`)) {
-        usersArr.push(userInput);
+function newCard(data) {
+  usersArr.push(userInput);
 
-          const avatarImageUrl = data.avatar_url;
-          const NumOfPublicRepo = data.public_repos;
-          const userUrl = data.html_url;
+  const avatarImageUrl = data.avatar_url;
+  const NumOfPublicRepo = data.public_repos;
+  const userUrl = data.html_url;
 
-          const newCard = document.createElement("a");
-          newCard.classList.add("card");
-          newCard.setAttribute("href", `${userUrl}`);
-          newCard.setAttribute("target", `_blank`);
+  const newCard = document.createElement("a");
+  newCard.classList.add("card");
+  newCard.setAttribute("href", `${userUrl}`);
+  newCard.setAttribute("target", `_blank`);
 
-          const newImg = document.createElement("img");
-          newImg.setAttribute("src", avatarImageUrl);
-          newCard.appendChild(newImg);
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", avatarImageUrl);
+  newCard.appendChild(newImg);
 
-          const newTitle = document.createElement("h2");
-          newTitle.innerText = userInput;
-          newCard.appendChild(newTitle);
+  const newTitle = document.createElement("h2");
+  newTitle.innerText = userInput;
+  newCard.appendChild(newTitle);
 
-          const newP = document.createElement("p");
-          newP.innerText = `Number of public repo’s: ${NumOfPublicRepo}`;
-          newCard.appendChild(newP);
+  const newP = document.createElement("p");
+  newP.innerText = `Number of public repo’s: ${NumOfPublicRepo}`;
+  newCard.appendChild(newP);
 
-          cardsContainer.appendChild(newCard);
-    }
+  cardsContainer.appendChild(newCard);
 }
 
 function errMsg(msg) {
-    errText.innerText = `Error- ${msg}`;
-    errText.classList.remove('hiddenText');
+  errText.innerText = `${msg}`;
+  errText.classList.remove("hiddenText");
 }
